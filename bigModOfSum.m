d116c581060897e33b6301d52ef97738ed91814e
chen-yu-ye <71300533+chen-yu-ye@users.noreply.github.com>
2020-12-11 14:55:09 +0800

    Add files via upload

---
+++ bigModOfSum.m
@@ -0,0 +1,24 @@
+sum = 0; %reset sum
+for ii = 1:10
+    sum = sum + bigMod(ii,ii,10^10); %sum up each ii^ii after bigMod
+    sum = mod(sum,10^10); %let the final result be last 10 digits
+end
+
+disp(sum);%show the result
+
+function y = bigMod(target,exp,modNum) %it's a recursive function
+    if exp == 0 %when exponential is 0, return 0
+        y = 1;
+    elseif exp == 1 %when exponential is 1, return the original number
+        y = target;
+    else 
+        y = bigMod(target,floor(exp/2),modNum); %this will be used later
+        if mod(exp,2) == 1
+            y = mod(y * y * target,modNum); %if exp is an odd number, multiply target ^ floor(exp/2) two time 
+        else                                % and multiply target,then mod 10^10. ex:11^11 = 11^5 * 11^5 * 11
+            y = mod(y * y,modNum);          %if exp is an odd number, multiply target ^ floor(exp/2) two time 
+        end                                 % ex:10^10 = 10^5 * 10^5
+    end
+end
+        
+
